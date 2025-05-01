6309ca4bd4d228adf2934aefc86d8679eb8afd45
Nikhil Chakravarthy Mallela <nikhilchakravarthy.mallela@gmail.com>
2025-05-01 18:13:48 +0530

    Add App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MockupViewer from './components/MockupViewer';
+
+function App() {
+  return (
+    <div>
+      <MockupViewer />
+    </div>
+  );
+}
+
+export default App;
